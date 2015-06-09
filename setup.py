b2df0ef2b5fc5a1ee0f993a5f5584ecb366a22a3
hanirok <corina.riba@upcnet.es>
2015-06-09 16:08:31 +0200

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the genweb.chineselanguagebar package."""
 
-version = '0.3'
+version = '0.4.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
